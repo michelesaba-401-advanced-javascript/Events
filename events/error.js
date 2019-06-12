e5ae52e0a9a7f7302790e1a08e65f29a0e04c01c
Michele Saba <michelemsaba@gmail.com>
2019-06-12 15:12:52 -0500

    file setup and error emitter and listener

---
+++ events/error.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const hub = require('./hub');
+
+hub.on('error', (err) => {
+    console.log("=> => Something went wrong", err);
+});
+
+
+
+// module.exports = {
+//   errorhandler
+// };
