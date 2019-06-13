b627c43175055ccef36f127fc13fcb531281ccb3
Michele Saba <michelemsaba@gmail.com>
2019-06-13 16:26:59 -0500

    added log emitter to file save function

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 'use strict';
+const hub = require('./events/hub');
+require('./events/error');
+require('./events/logger');
+require('./events/network-logger');
 
 const fs = require('fs');
 
 const alterFile = (file) => {
   fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
+    if(err) { 
+      hub.emit('error', err);
+    }
     let text = data.toString().toUpperCase();
     fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
+      if(err) { 
+        hub.emit('error', err);
+        hub.emit('success', file);
+        hub.emit('save', file);
+      }
     });
   });
 };
