b13c8b673b274c46857cdb21b6ff07d9fd84d007
Michele Saba <michelemsaba@gmail.com>
2019-06-13 16:01:10 -0500

    added async await to readfile function

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 'use strict';
-const hub = require('./events/hub');
-require('./events/error');
-require('./events/logger');
 
 const fs = require('fs');
 
 const alterFile = (file) => {
   fs.readFile( file, (err, data) => {
-    if(err) { 
-      hub.emit('error', err);
-    }
+    if(err) { throw err; }
     let text = data.toString().toUpperCase();
     fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { 
-        hub.emit('error', err);
-        hub.emit('success', file);
-      }
+      if(err) { throw err; }
+      console.log(`${file} saved`);
     });
   });
 };
